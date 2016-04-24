43806c069466409061f7ed95b5089d4cc3cbcf44
Sam Thomas <sgpthomas@gmail.com>
2016-04-23 17:58:24 -0700

    moved to python setuptools

--- install.sh
+++
@@ -1,3 +0,0 @@
-echo "Installing pydl"
-cp -R pydl /Users/laptag/anaconda/lib/python3.5/site-packages
-echo "Installed pydl"
---
+++ setup.py
@@ -0,0 +1,19 @@
+import os
+from setuptools import setup
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pydl",
+    version = "0.1.0",
+    author = "Samuel Thomas",
+    author_email = "sgpthomas@gmail.com",
+    description = ("A python library designed to make analyzing timeseries data easy."),
+    packages=['pydl'],
+    long_description=read('README.md'),
+)
